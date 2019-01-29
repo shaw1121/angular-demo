83fb4e0f2cfa60e46ef3a30141d1e2dfa3efa1ce
ZhangXiao <xiao-zhang@siemens.com>
2019-01-29 15:55:10 +0800

    modify router module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,11 +2,16 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { CommonModule } from '@angular/common';
 import { HeroesComponent } from './heroes/heroes.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { HeroDetailComponent } from './hero-detail/hero-detail.component';
 
 const routes: Routes = [
   // path: 一个用于匹配浏览器地址栏中 URL 的字符串
   // component: 当导航到此路由时，路由器应该创建哪个组件
-  {path: 'heroes', component: HeroesComponent},
+  { path: 'heroes', component: HeroesComponent },
+  { path: 'dashboard', component: DashboardComponent },
+  { path: '', redirectTo: './dashboard', pathMatch: 'full' },
+  { path: 'detail/:id', component: HeroDetailComponent },
 ];
 
 @NgModule({
